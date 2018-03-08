44927cafe5ee3af87a0c85643a5bd2a0b753aaa5
pskchang <pschang@ucsd.edu>
2018-03-08 13:53:09 -0800

    Added all my background pictures

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,4 +1,17 @@
 let schedule;
+let mbg = document.createElement("img");
+mbg.src = "mbg.jpg";
+let tbg = document.createElement("img");
+tbg.src = "tbg.jpg";
+let wbg = document.createElement("img");
+wbg.src = "wbg.jpg";
+let thbg = document.createElement("img");
+thbg.src = "thbg.jpg";
+let fbg = document.createElement("img");
+fbg.src = "fbg.jpg";
+let sbg = document.createElement("img");
+sbg.src = "sbg.jpg";
+let bgArr = [mbg.src, tbg.src, wbg.src, thbg.src, fbg.src, sbg.src];
 let container = document.getElementById("container");
 $(document).on('ready', () => {
     $.ajax({
@@ -6,6 +19,9 @@ $(document).on('ready', () => {
         url: 'http://slack-server.elasticbeanstalk.com/calendar/LA/20',
         success: function (data, status) {
             var currentTime = new Date();
+            let wkDay = currentTime.getDay();
+            console.log(wkDay);
+            document.body.style.backgroundImage = `url(${bgArr[wkDay-1]})`;
             let currDate = currentTime.toString().slice(4, 15);
             let counter = 0;
             while (currDate !== Object.entries(data)[counter][0]) {
@@ -16,6 +32,8 @@ $(document).on('ready', () => {
                 let today = Object.entries(data)[i][1];
                 let newDate = document.createElement("span");
                 newDate.className = "date";
+               
+                
                 
                 for (let k = 0; k < today.length; k++) {
                     let myData = Object.entries(data)[i][1][k];
@@ -31,6 +49,7 @@ $(document).on('ready', () => {
                     newEvent.appendChild(eventTime);
                     newEvent.appendChild(summ);
                     dayBox.append(newEvent);
+                    
                    
                 }
                 if (dayBox.id == "day1") {
